3090745e2e7c842e26209ad72878d0408a74adef
the-iconic-rihan <rihanbagwan5017@gmail.com>
2021-03-10 11:38:44 -0800

    code updated

--- main.py
+++ main.py
@@ -7,8 +7,10 @@
 # pygame module to play audio
 
 # importing in-built  libraries
+from plyer import notification
 from pygame import mixer
 from time import time
+
 import datetime
 
 
@@ -35,24 +37,42 @@ def start_game():
         eye_secs = 5
         while True:
             if time() - init_water > water_secs:
+                notification.notify(
+                    title="Its water drinking time!!",
+                    message="Drink water helps to keep you hydrated.",
+                    app_icon="C:\\Users\\Lenovo\\PycharmProjects\\desktopNotifier\\glass.ico",
+                    timeout=5
+                )
                 print("Its water drinking time, enter drank to stop the music!")
                 musicloop("musicbyaden-jurgance-beach.mp3", "drank")
                 init_water = time()
+
                 log_now("Drank water at : ")
             elif time() - init_exercise > exercise_secs:
+                notification.notify(
+                    title="Its time to do some exercise!!",
+                    message="Regular exercise keeps your body fit .",
+                    app_icon="D:\\projects\\PycharmProjects\\healthprogrammer\\Icons8-Windows-8-Sports-Exercise.ico",
+                    timeout=5
+                )
                 print("its time to move your bones!")
                 musicloop("TouMouX-HardJump.mp3", "Done")
                 init_exercise = time()
                 log_now("Eyes relax  at : ")
 
             elif time() - init_eyes > eye_secs:
+                notification.notify(
+                    title="Its time to blink your eyes!!",
+                    message="Keep blinking your Eyelids to avoid problems.",
+                    app_icon="D:\\projects\\PycharmProjects\\healthprogrammer\\Itzikgur-My-Seven-Favorities.ico",
+                    timeout=5
+                )
                 print("Its time to blink your eyes!")
                 musicloop("Cartoon-eye-blink-sound-effect-free-download.mp3", "Done")
                 init_eyes = time()
                 log_now("Physical activity done at : ")
 
-            else:
-                menu()
+
 # clear log function
 def clear():
     f = open("mylogs.txt", "r+")
@@ -99,7 +119,6 @@ def log_now(sms):
         string += str("\t") + str(sms) + str(" ") + str(datetime.datetime.now())
         f.write(string)
     print("Information added successfully!!!")
-
 
 menu()
 start_game()
